1b41f755541170afe044f457528123a834ec3fec
arshqiii <marshq23@gmail.com>
2025-01-16 20:32:57 +0700

    made slider functionable

--- script.js
+++ script.js
@@ -14,10 +14,38 @@ var randomBtn = document.querySelector("#randomBtn");
 var eraserBtn = document.querySelector("#eraserBtn");
 var resetBtn = document.querySelector("#resetBtn");
 var board = document.querySelector(".board");
+var sizeSlider = document.querySelector(".slider");
+var sizeDisplay = document.querySelector(".gridSize");
+var colorPicker = document.querySelector("#colorPicker");
+
+colorBtn.addEventListener("click", function(e) {
+    curMode = "color";
+});
+
+randomBtn.addEventListener("click", function(e) {
+    curMode = "random";
+});
+
+eraserBtn.addEventListener("click", function(e) {
+    curMode = "eraser";
+});
+
+colorPicker.addEventListener("click", function(e) {
+    curColor = colorPicker.value;
+});
+
+sizeSlider.addEventListener("input", function(e){
+    curSize = sizeSlider.value;
+    sizeDisplay.innerHTML = `${curSize} x ${curSize}`;
+    setBoard(curSize);
+})
+
 
 function setBoard(n) {
     board.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+
+    board.innerHTML = "";
 
     for (var i=0;i<n*n;i++){
         const square = document.createElement("div");
@@ -25,6 +53,16 @@ function setBoard(n) {
         board.insertAdjacentElement("beforeend", square);
     }
 }
+
+function setMode(){
+
+}
+
+
+
+
+
+
 
 
 
