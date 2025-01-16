70c08a55bee1d230494984779cc92661291a418d
arshqiii <marshq23@gmail.com>
2025-01-16 18:32:31 +0700

    made board

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+//* DEFAULT SETTING
 const defaultMode = "color";
 const defaultColor = "black";
 const defaultSize = 16;
 
+//* CURRENT SETTING
 var curMode = defaultMode;
 var curColor = defaultColor;
 var curSize = defaultSize;
 
+//* INSTANTIATE OBJECTS FROM DOCUMENT
 var colorBtn = document.querySelector("#colorBtn");
 var randomBtn = document.querySelector("#randomBtn");
 var eraserBtn = document.querySelector("#eraserBtn");
 var resetBtn = document.querySelector("#resetBtn");
 var board = document.querySelector(".board");
+
+function setBoard(n) {
+    board.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+
+    for (var i=0;i<n*n;i++){
+        const square = document.createElement("div");
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+
+
+setBoard(defaultSize);
 
