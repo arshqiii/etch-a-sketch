81e1019b8371932b7339403cd3787bb84768f509
arshqiii <marshq23@gmail.com>
2025-01-16 17:32:04 +0700

    setup script

---
+++ script.js
@@ -0,0 +1,14 @@
+const defaultMode = "color";
+const defaultColor = "black";
+const defaultSize = 16;
+
+var curMode = defaultMode;
+var curColor = defaultColor;
+var curSize = defaultSize;
+
+var colorBtn = document.querySelector("#colorBtn");
+var randomBtn = document.querySelector("#randomBtn");
+var eraserBtn = document.querySelector("#eraserBtn");
+var resetBtn = document.querySelector("#resetBtn");
+var board = document.querySelector(".board");
+
